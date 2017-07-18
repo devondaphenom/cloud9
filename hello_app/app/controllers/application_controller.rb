01361ac2b83b86f6094eea63dec40f0e03d4038a
devondaphenom <dseth.johnson@gmail.com>
2017-07-18 21:18:32 +0000

    Toy_app

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "¡Hola, mundo!"
+    render html: "Hello World! || ¡Hola, mundo!"
   end
    def goodbye
     render html: "Goodbye, world!"
-end
+  end
 end
